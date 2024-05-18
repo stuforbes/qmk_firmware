d853ae207cf0afe480f1999d438d25871fb6f131
Stu Forbes <stu@stuforbes.com>
2024-05-18 11:00:56 +0100

    Tidying up formatting of layouts

--- keyboards/sofle_choc/keymaps/stuforbes/keymap.c
+++ keyboards/sofle_choc/keymaps/stuforbes/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Ins  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
- * |      |	 |	|      |      |      |       |    |       | +Lsft| +Lctl| +Lgui| +Lalt|      |      |
+ * |      |	 |	|      |      |      |       |    |       |       | +Lsft| +Lctl| +Lgui| +Lalt|      |      |
  * |------+------+------+------+------+------|  Mute |    | Pause |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -39,18 +39,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_QWERTY] = LAYOUT( \
-    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_GRV,
-    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_INS,
-KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       MT(MOD_LSFT, KC_H),MT(MOD_LCTL,KC_J),MT(MOD_LGUI,KC_K),MT(MOD_LALT,KC_L),KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_MPLY,KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                      KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_ENT,    KC_SPC, MO(2),  KC_BSPC,  KC_DEL, KC_RCTL
+    KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,              KC_7,             KC_8,             KC_9,             KC_0,    KC_GRV,
+    KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,              KC_U,             KC_I,             KC_O,             KC_P,    KC_INS,
+    KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       MT(MOD_LSFT, KC_H),MT(MOD_LCTL,KC_J),MT(MOD_LGUI,KC_K),MT(MOD_LALT,KC_L),KC_SCLN, KC_QUOT,
+    KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,   KC_MPLY,  KC_N,              KC_M,             KC_COMM,          KC_DOT,           KC_SLSH, KC_RSFT,
+                      KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_ENT,    KC_SPC,   MO(2),             KC_BSPC,          KC_DEL,           KC_RCTL
 ),
 
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  `   |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  | PgUp | Home |  End |   R  |   T  |                    |   ;  |   `  |   -  |   [  |   ]  | F12  |
+ * | Tab  | PgUp | Home |  End |      |      |                    |   ;  |   `  |   -  |   [  |   ]  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  | PgDn | MsL  | MsD  | MsU  | MsR  |-------.    ,-------| Left | Down |  Up  | Right|   o  |   '  |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
@@ -61,11 +61,11 @@ KC_ESC,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       MT(MOD_
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT(
-    KC_GRV,  KC_F1,    KC_F2,      KC_F3,      KC_F4,    KC_F5,   			     KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
-    KC_TAB,  KC_PGUP, KC_HOME,    KC_END,     KC_R,     KC_T,                               KC_GRV,  KC_SCLN, KC_MINS, KC_LBRC,  KC_RBRC, KC_F12,
-    KC_ESC,  KC_PGDN, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_O,    KC_QUOT,
-    KC_LSFT, KC_CAPS, KC_MS_BTN1, KC_MS_BTN2, _______,  _______,      _______,     _______, KC_EQL,  KC_BSLS, KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
-                       KC_LGUI,    KC_LALT,    KC_LCTL,  KC_TRNS,      KC_ENT,      KC_SPC,  KC_TRNS, KC_RCTL, KC_RALT, KC_RCTL
+    KC_GRV,  KC_F1,   KC_F2,      KC_F3,      KC_F4,    KC_F5,   			                 KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,
+    KC_TAB,  KC_PGUP, KC_HOME,    KC_END,     _______,  _______,                             KC_GRV,  KC_SCLN, KC_MINS, KC_LBRC,  KC_RBRC, KC_F12,
+    KC_ESC,  KC_PGDN, KC_MS_LEFT, KC_MS_DOWN, KC_MS_UP, KC_MS_RIGHT,                         KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_O,    KC_QUOT,
+    KC_LSFT, KC_CAPS, KC_MS_BTN1, KC_MS_BTN2, _______,  _______,      _______,      _______, KC_EQL,  KC_BSLS, KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT,
+                      KC_LGUI,    KC_LALT,    KC_LCTL,  KC_TRNS,      KC_ENT,       KC_SPC,  KC_TRNS, KC_RCTL, KC_RALT, ;KC_RCTL
 ),
 };
 
